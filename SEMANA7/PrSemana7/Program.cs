ae70098260bb5885af97c87a679b63366a4f8a9a
Esdras <edsantiago@correo.url.edu.gt>
2024-03-01 11:13:02 -0600

    Actualizacion semana 7

---
+++ SEMANA7/PrSemana7/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SEMANA7PRACTICA
+{
+    class SEMANA7ESDRAS
+    {
+        static void Main(string[] args)
+        {
+            string value1 = " a";
+            string value2 = "Esdras ";
+            string texto;
+            string palabra = "";
+
+            /* Trim quita el espacio en blanco
+               ToLower convierte de mayuscula a minuscula el texto */
+            Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+            Console.WriteLine("Hola, ingrese texto");
+            texto = Console.ReadLine();
+            Console.WriteLine("ingrese palabra a comparar");
+            palabra = Console.ReadLine();
+
+            Console.WriteLine("Contains: " + texto.Contains("Esdras"));
+            Console.WriteLine("Starts With: " + texto.StartsWith("Esdras"));
+            Console.WriteLine("Ends Whit: " + texto.EndsWith("Esdras"));
+            Console.ReadKey();
+        }
+    }
+}
