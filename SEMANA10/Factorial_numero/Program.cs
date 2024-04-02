2119311200b30836f5a8cf1a691c137b8aabf6de
Esdras <edsantiago@correo.url.edu.gt>
2024-04-02 07:57:18 -0600

    Actualizacion hasta semana 12

---
+++ SEMANA10/Factorial_numero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace factorilNumero{
+    class Program{
+        public static void Main(string[] args){
+            while(true){
+            Console.Write("Ingrese un numero para conocer el valor de su factorial: ");
+            int numero = int.Parse(Console.ReadLine());
+            int Factorial = CalcularFactorial(numero);
+            Console.WriteLine($"El factorial de {numero} es {CalcularFactorial(numero)}");
+            }
+        }
+
+        public static int  CalcularFactorial(int numero){
+            if (numero == 0 ){
+                return 1;
+            }else{
+                int factorial = 1;
+                for(int i = 1; i <= numero; i++){
+                    factorial *= i;
+                }
+                    return factorial;
+            }
+
+        }
+    }
+}
