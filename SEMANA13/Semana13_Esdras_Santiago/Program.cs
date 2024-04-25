c6cc7675d77ad8da5d922129afc60cb32bc0ae8b
Esdras <edsantiago@correo.url.edu.gt>
2024-04-24 21:54:16 -0600

    prueba de pull reques para reposicion de puntos

---
+++ SEMANA13/Semana13_Esdras_Santiago/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Semana13{
+    class Program{
+ class ArregloUnidimensional
+    {
+        /* Programa arreglos_1:
+
+a) solicitar el ingreso de 8 números
+
+b) guardar en un arreglo
+
+c) mostrar los 8 números ingresados
+
+d) mostrar la suma de los 8 números
+
+e) mostrar el promedio de los 8 números */
+        private int[] notas;
+
+        //Metodo cargar
+        public void Cargar()
+        {
+            //Asignando a arreglo un tamaño 5
+            notas = new int[8];
+
+            //Declaración de iteración For
+            for(int i = 0; i < notas.Length; i++)
+            {
+                Console.Write($"Ingrese valor para posición {i+1}: ");
+                notas[i] = int.Parse(Console.ReadLine()??string.Empty);
+            }
+        }
+
+        //Metodo para mostrar arreglo
+        public void Imprimir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Lectura de arreglo en posición: ");
+            //Declaración de iteración For
+            for(int im= 0; im < notas.Length; im++){
+                Console.WriteLine($"{im}: {notas[im]}");
+            }
+            Console.ReadKey();
+        }
+
+        public void sumaNumeros(){
+            int resultadoSuma = 0;
+            Console.WriteLine("");
+            Console.WriteLine($"Resultado de suma {notas.Length} numeros: ");
+
+            foreach(int n in notas){
+                resultadoSuma += n;
+            }
+            Console.Write(resultadoSuma);
+        }
+
+        public void promedioNumeros(){
+            int resultadoPromedio = 0;
+            Console.WriteLine("");
+            Console.WriteLine($"Resultado promedio {notas.Length} numeros: ");
+
+            foreach(int p in notas){
+                resultadoPromedio += p;
+            }
+            Console.Write(resultadoPromedio/notas.Length);
+        }
+
+        // Metodo principal
+        static void Main()
+        {
+            ArregloUnidimensional arregloUnidimensional_ = new ArregloUnidimensional();
+            arregloUnidimensional_.Cargar();
+            arregloUnidimensional_.Imprimir();
+            arregloUnidimensional_.sumaNumeros();
+            arregloUnidimensional_.promedioNumeros();
+        }
+    }
+    }
+}
---
+++ SEMANA15/semana15_Esdras_Santiago/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Santiago
+{
+int numero;
+int numero1;
+string cadena;
+public static void Main()
+{
+    Santiago objetoSantiago = new Santiago();
+}
+Santiago(int numero, string cadena, int numero1) {
+this.numero = numero;
+this.cadena = cadena;
+this.numero1 = numero1;
+}
+void show() {
+Console.WriteLine(this.numero);
+Console.WriteLine(this.cadena);
+}
+
+void ingresoParams(){
+    Console.WriteLine("Ingrse un numero entero: ");
+    int params1 = int.Parse(Console.ReadLine()??string.Empty);
+    Console.WriteLine("Ingrese una cadena de caracteres: ");
+    string? params2 = Console.ReadLine();
+    Console.WriteLine("Ingrse otro numero entero: ");
+    int params3 = int.Parse(Console.ReadLine()??string.Empty);
+    Santiago objetoSantiago = new Santiago(params1, params2, params3);
+}
+
+}
+
