f282c39cd268e6b8752e90cc751f7c72c36db564
Esdras <edsantiago@correo.url.edu.gt>
2024-04-26 07:13:16 -0600

    Actualizacion para pull-request

--- SEMANA15/semana15_Esdras_Santiago/Program.cs
+++ SEMANA15/semana15_Esdras_Santiago/Program.cs
@@ -4,9 +4,14 @@ public class Santiago
 int numero;
 int numero1;
 string cadena;
+bool cierreProgram = true;
 public static void Main()
 {
-    Santiago objetoSantiago = new Santiago();
+    Santiago santiago = new Santiago(0,"",0);
+    while(santiago.cierreProgram){
+        Console.Clear();
+        santiago.menu();
+    }
 }
 Santiago(int numero, string cadena, int numero1) {
 this.numero = numero;
@@ -14,19 +19,48 @@ this.cadena = cadena;
 this.numero1 = numero1;
 }
 void show() {
-Console.WriteLine(this.numero);
-Console.WriteLine(this.cadena);
+Console.WriteLine($"Los valores ingresados por el usario son Numero1:{numero}, Numero2:{this.numero1}, String:{this.cadena}");
+Console.ReadKey();
 }
 
-void ingresoParams(){
-    Console.WriteLine("Ingrse un numero entero: ");
+void nuevo(){
+    Console.WriteLine("Ingrese un numero entero: ");
     int params1 = int.Parse(Console.ReadLine()??string.Empty);
     Console.WriteLine("Ingrese una cadena de caracteres: ");
     string? params2 = Console.ReadLine();
     Console.WriteLine("Ingrse otro numero entero: ");
     int params3 = int.Parse(Console.ReadLine()??string.Empty);
     Santiago objetoSantiago = new Santiago(params1, params2, params3);
+    objetoSantiago.menu();
 }
 
+void menu(){
+    Console.WriteLine("1.Ingreso de valores\n2.Mostrar valores\n3.Salir del programa");
+    Console.WriteLine("Seleccione una de las 3 opciones, indicando el numero de opcion");
+    string? opcion = Console.ReadLine();
+    int opcionInt;
+    int.TryParse(opcion, out opcionInt);
+    switch(opcionInt){
+        case 1:
+        Console.Clear();
+        nuevo();
+        break;
+
+        case 2:
+        Console.Clear();
+        show();
+        break;
+
+        case 3:
+        Console.Clear();
+        cierreProgram = false;
+        break;
+
+        default:
+        Console.Clear();
+        Console.WriteLine("Valor invalido");
+        break;
+    }
+    }
 }
 
